d2d0a26ee3c8b8f42d8f1afffa7502547a7c0233
taggertt <taggertt@mail.ru>
2023-01-18 21:45:42 +0600

    rework gradle, .appveyor

---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,59 @@
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int change) {
+        return LocalDate.now().plusDays(change).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String[] cities = new String[]{"Абакан", "Анадырь", "Архангельск", "Астрахань", "Барнаул", "Белгород",
+                "Биробиджан", "Благовещенск", "Брянск", "Великий  Новгород", "Владивосток", "Владикавказ",
+                "Владимир", "Волгоград", "Вологда", "Воронеж", "Гатчина", "Горно-Алтайск", "Грозный",
+                "Екатеринбург", "Иваново", "Ижевск", "Иркутск", "Йошкар-Ола", "Казань", "Калининград", "Калуга",
+                "Кемерово", "Киров", "Кострома", "Красногорск", "Краснодар", "Красноярск", "Курган", "Курск",
+                "Кызыл", "Липецк", "Магадан", "Магас", "Майкоп", "Махачкала", "Москва", "Мурманск", "Нальчик",
+                "Нарьян-Мар", "Нижний Новгород", "Новосибирск", "Омск", "Орёл", "Оренбург", "Пенза", "Пермь",
+                "Петрозаводск", "Петропавловск-Камчатский", "Псков", "Ростов-на-Дону", "Рязань", "Салехард",
+                "Самара", "Санкт-Петербург", "Санкт-Петербург", "Саранск", "Саратов", "Севастополь", "Симферополь",
+                "Смоленск", "Ставрополь", "Сыктывкар", "Тамбов", "Тверь", "Томск", "Тула", "Тюмень", "Улан-Удэ",
+                "Ульяновск", "Уфа", "Хабаровск", "Ханты-Мансийск", "Чебоксары", "Челябинск", "Черкесск", "Чита",
+                "Элиста", "Южно-Сахалинск", "Якутск", "Ярославль"};
+        return cities[faker.number().numberBetween(0, cities.length) - 1];
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    @Value
+    public static class RegistrationInfo {
+        String name;
+        String city;
+        String phone;
+
+        public RegistrationInfo(String name, String city, String phone) {
+            this.name = name;
+            this.city = city;
+            this.phone = phone;
+        }
+
+        public static RegistrationInfo generateInfo(String locale) {
+            return new RegistrationInfo(generateName(locale), generateCity(locale), generatePhone(locale));
+        }
+    }
+}
---
+++ src/test/java/TestDelivery.java
@@ -0,0 +1,50 @@
+import lombok.Data;
+import lombok.val;
+import lombok.var;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.util.Date;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+ class TestDelivery {
+
+    @BeforeEach
+    void setup () {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSuccessfulTwoMeetings() {
+        DataGenerator.RegistrationInfo userInfo = DataGenerator.RegistrationInfo.generateInfo("ru");
+        int daysAddToFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysAddToFirstMeeting);
+        int daysAddToSecMeeting = 10;
+        String secMeetingDate = DataGenerator.generateDate(daysAddToSecMeeting);
+        $("[placeholder='Город']").setValue(userInfo.getCity());
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL + "A" + Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(firstMeetingDate);
+        $("[data-tes-id='name'] input").setValue(userInfo.getName());
+        $("[name='phone']").setValue(userInfo.getPhone());
+        $(".checkbox__control").click();
+        $$("[role='button']").find(exactText("Запланировать")).click();
+        $("[data-test-id='success-notification']  .notification__title")
+                .shouldBe(visible, Duration.ofSeconds(10)).shouldHave(exactText("Успешно!"));
+        $("[data-test-id='success-notification']  .notification__content")
+                .shouldBe(visible, Duration.ofSeconds(10)).shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A" + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(secMeetingDate);
+        $$("[role='button']").find(exactText("Запланировать")).click();
+        $("[data-test-id='replan-notification']  .notification__title").shouldBe(visible, Duration.ofSeconds(10)).shouldHave(exactText("Необходимо подтверждение"));
+        $$("[data-test-id='replan-notification'] button").find(exactText("Перепланировать")).click();
+        $("[data-test-id='success-notification']  .notification__title").shouldBe(visible, Duration.ofSeconds(10)).shouldHave(exactText("Успешно!"));
+        $("[data-test-id='success-notification']  .notification__content").shouldBe(visible, Duration.ofSeconds(10)).shouldHave(exactText("Встреча успешно запланирована на " + secMeetingDate));
+    }
+}
+
+
